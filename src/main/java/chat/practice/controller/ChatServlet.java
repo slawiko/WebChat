747aaa8b7a5df63ef11d1b2e974d586bde05dca8
slawiko <shchaurouski.slava@gmail.com>
2015-04-22 22:47:40 +0300

    ChatServlet class creating

--- src/main/java/chat/practice/controller/ChatServlet.java
+++ src/main/java/chat/practice/controller/ChatServlet.java
@@ -1,7 +1,42 @@
 package chat.practice.controller;
 
-/**
- * Created by Shchaurouski.slava on 21.04.2015.
- */
-public class ChatServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import chat.practice.model.Message;
+import chat.practice.model.MessageStorage;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+@WebServlet("/chat")
+public class ChatServlet extends HttpServlet {
+
+
+
+	@Override
+	public void init() throws ServletException {
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+
+	}
 }
