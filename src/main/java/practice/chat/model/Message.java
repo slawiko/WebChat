fae5c138b864ff424ba0590228bd25c057afd5fc
slawiko <shchaurouski.slava@gmail.com>
2015-04-27 09:31:56 +0300

    field "date" creating

--- src/main/java/practice/chat/model/Message.java
+++ src/main/java/practice/chat/model/Message.java
@@ -1,37 +1,49 @@
 package practice.chat.model;
 
 import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static practice.chat.util.MessageUtil.DATE_FORMAT;
 
 public class Message {
     private String id;
     private String author;
     private String text;
+    private String date;
 
-    public Message() {
-        this.id = "";
-        this.author = "";
-        this.text = "";
+    private Message() {
+    }
+
+    public Message(String id, String author, String text, String date) {
+        this.id = id;
+        this.author = author;
+        this.text = text;
+        this.date = date;
     }
 
     public Message(String id, String author, String text) {
         this.id = id;
-        this.author = author;
         this.text = text;
+        this.author = author;
+        this.date = setDate();
     }
 
     public Message(String author, String text) {
+        this.id = uniqueId();
         this.text = text;
         this.author = author;
-        this.id = uniqueId();
+        this.date = setDate();
     }
 
     public Message(Message message) {
         this.id = message.getId();
         this.author = message.getAuthor();
         this.text = message.getText();
+        this.date = message.getDate();
     }
 
-    public String uniqueId() {
+    private String uniqueId() {
         return UUID.randomUUID().toString();
     }
 
@@ -58,5 +70,14 @@ public class Message {
     }
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+    private String setDate() {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+        return format.format(date);
     }
 }
