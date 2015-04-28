43c0922afc6479f1cf0576edcdff93e5750cb13a
slawiko <shchaurouski.slava@gmail.com>
2015-04-28 12:06:40 +0300

    integration with XMLStorage

--- src/main/java/practice/chat/util/ServletUtil.java
+++ src/main/java/practice/chat/util/ServletUtil.java
@@ -1,20 +1,18 @@
 package practice.chat.util;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.xml.sax.SAXException;
 
-import practice.chat.storage.MessageStorage;
 import practice.chat.storage.XMLStorage;
 import practice.chat.model.Message;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 import static practice.chat.util.MessageUtil.ID;
@@ -25,9 +23,13 @@ import static practice.chat.util.MessageUtil.DATE;
 import static practice.chat.util.MessageUtil.getToken;
 
 public final class ServletUtil {
-	public  static final String APPLICATION_JSON = "application/json";
-	public  static final String TOKEN = "token";
-	public  static final String DELETED = "DELETED";
+	public static final String APPLICATION_JSON = "application/json";
+	public static final String TOKEN = "token";
+	public static final String DELETED = "DELETED";
+	public static final String POST = "post";
+	public static final String DELETE = "delete";
+	public static final String PUT = "put";
+	public static final String GET = "get";
 	private static final String MESSAGES = "messages";
 
 	private ServletUtil() {
@@ -43,42 +45,19 @@ public final class ServletUtil {
 		return sb.toString();
 	}
 
-	public static String getServerResponse(int index) {
+	public static String getServerResponse(int index) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
 		JSONObject jsonObject = new JSONObject();
-		jsonObject.put(MESSAGES, MessageStorage.getSubMessagesByIndex(index));
-		jsonObject.put(TOKEN, getToken(MessageStorage.getSize()));
-		return jsonObject.toJSONString();
-	}
-
-	@SuppressWarnings("unchecked")
-	public static String formResponse(int index) throws ParserConfigurationException, SAXException, IOException {
-		List<Message> messages = MessageStorage.getSubMessagesByIndex(index);
-		JSONObject jsonObject = new JSONObject();
-		JSONArray ArrayMessages = new JSONArray();
-		for(int i = 0; i < messages.size(); i++) {
-			JSONObject elementOfArray = new JSONObject();
-			elementOfArray.put(ID, messages.get(i).getId());
-			if(!messages.get(i).getText().isEmpty()) {
-				elementOfArray.put(TEXT, messages.get(i).getText());
-			}
-			if(!messages.get(i).getAuthor().isEmpty()) {
-				elementOfArray.put(DATE, messages.get(i).getDate());
-				elementOfArray.put(AUTHOR, messages.get(i).getAuthor());
-			}
-			ArrayMessages.add(elementOfArray);
-		}
-		jsonObject.put(MESSAGES, ArrayMessages);
+		jsonObject.put(MESSAGES, XMLStorage.getSubNodeList(index));
 		jsonObject.put(TOKEN, getToken(XMLStorage.getStorageSize()));
 		return jsonObject.toJSONString();
 	}
 
 	public static void loadHistory() throws SAXException, IOException, ParserConfigurationException, TransformerException {
 		if (XMLStorage.isExist()) {
-			List<Message> messages = XMLStorage.getMessages();
-			for(Message i : messages) {
-				System.out.println("Прочитано сообщение из history.xml: " + i.getDate() + " [" + i.getAuthor() + "] : " + i.getText());
+			List<Message> messages = XMLStorage.getListMessages();
+			for(Message message : messages) {
+				System.out.println("Read a message from history.xml: " + message.getDate() + " {" + message.getAuthor() + "} : {" + message.getText() + "}" );
 			}
-			MessageStorage.addAll(messages);
 		} else {
 			XMLStorage.createStorage();
 		}
