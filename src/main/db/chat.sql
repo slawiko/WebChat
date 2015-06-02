dffe973f29373da256aec22ba632583cbd9905f7
shchaurouski.slava <shchaurouski.slava@gmail.com>
2015-06-02 13:20:58 +0300

    tables format changed

--- src/main/db/chat.sql
+++ src/main/db/chat.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `id` char(255) NOT NULL,
+  `author` char(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authors`
+--
+
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `messages`
 --
 
@@ -23,13 +46,13 @@ DROP TABLE IF EXISTS `messages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messages` (
-  `id` char(36) NOT NULL DEFAULT '',
+  `id` char(255) NOT NULL,
+  `author_id` char(255) NOT NULL,
   `text` char(255) DEFAULT NULL,
-  `date` char(255) DEFAULT NULL,
-  `user_id` char(255) DEFAULT NULL,
+  `date` char(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,32 +62,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES ('37511d9b-74d6-45ac-bf19-5ca04be71ff7','Hello','11-05-2015 20:04','1'),('6762f2eb-8d26-4ace-aeb3-30368ddcec5b','Hi','19-05-2015 22:23','0'),('a144a9ae-85f2-4443-8e37-145c1cc3c091','How are you?','21-05-2015 21:21','1'),('e66a20b0-fde6-4973-a3df-f1beac2b9629','And you?','21-05-2015 21:25','0'),('ea8555f1-8868-47b0-97d8-7f9d9aab5350','I\'m fine','21-05-2015 21:24','0'),('k69a20b0-fde6-7973-e5df-f1itac2b9629','Me too','21-05-2015 21:27','1'),('k69a20b0-fye6-7253-95df-f1it62kfi629','lalala','21-05-2015 21:29','0'),('k6poupb0-fye6-2253-95df-f1it62222229','iopoiw','21-05-2015 21:45','1'),('k6[p[ib0-pme6-po53-9klf-f3333jg22229','hw3k4j;elfpr','21-05-2015 21:48','0'),('l0opiib0-pme6-po53-9klg-odpe3jg0pfo9','poiwoitughj3','21-05-2015 21:54','1');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `id` char(36) NOT NULL,
-  `name` char(40) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('0','slawik'),('1','maz');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -76,4 +74,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-26 12:16:09
+-- Dump completed on 2015-06-02 12:39:20
